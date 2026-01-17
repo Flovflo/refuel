8d33684bc1c2eacd218285018e6bf0e6bb697f05
Florian Taffin <floriantaffin@gmail.com>
2026-01-17 20:37:24 +0100

    Initial Commit

---
+++ refuel/refuelApp.swift
@@ -0,0 +1,17 @@
+//
+//  refuelApp.swift
+//  refuel
+//
+//  Created by Florian Taffin on 17/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct refuelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ refuelTests/refuelTests.swift
@@ -0,0 +1,17 @@
+//
+//  refuelTests.swift
+//  refuelTests
+//
+//  Created by Florian Taffin on 17/01/2026.
+//
+
+import Testing
+@testable import refuel
+
+struct refuelTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
